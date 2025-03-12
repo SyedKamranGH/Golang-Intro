b1004fb02d8c91a3cd590e266112d7d687430016
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-03-12 19:57:31 +0500

    Basic Go variables

---
+++ tutorial.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Quiz game! ")
+
+	var name string
+
+	fmt.Printf("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println(name)
+
+	var age uint //unasigne int cant be negative like age else have int , float64
+
+	fmt.Printf("Enter your age: ")
+	fmt.Scan(&age)
+
+	fmt.Println(age >= 10)
+	// var isEmployed = true
+
+	position := "Full Stack"
+	// fmt.Println(name)
+	// fmt.Println(age)
+	// fmt.Println(isEmployed)
+	// fmt.Println(position)
+
+	// position = "Go-Lang"
+	if age >= 10 {
+		fmt.Printf("Hi %v, Lets start becoming %v dev", name, position)
+	}
+}
