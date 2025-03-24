292dbe85702d4da453415bab1482a3124a28930a
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-03-24 18:56:30 +0500

    function delartion, defination and arguments

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// var GoLangLearning = "Learning GoLang step by step"
+// var taskItems = []string{GoLangLearning, "Welcome to our Todo app!", "Watching go crash course!", "Next is GoLang full course!"}
+
 func main() {
 	fmt.Println("~~~~~ Welcome to GoLang! ############")
 
@@ -17,12 +20,35 @@ func main() {
 
 	// Arrary have fix size and slices is unlimited array
 	var taskItems = []string{GoLangLearning, "Welcome to our Todo app!", "Watching go crash course!", "Next is GoLang full course!"}
+	maxItem := 45
+	// fmt.Println("ToDo App:", taskItems)
 
-	fmt.Println("ToDo App:", taskItems)
+	printTasks(taskItems, maxItem)
+
+	println("----------------")
+	taskItems = addTask(taskItems, "Go for web dev")
+	taskItems = addTask(taskItems, "Go for web dev with react app")
+
+	println("----------------")
+
+	printTasks(taskItems, maxItem)
+
+}
+
+func printTasks(taskItems []string, itemLimit int) {
+	fmt.Println("List of my Tasks")
 
 	for index, task := range taskItems {
 		// fmt.Println(index+1, ".", task)
 		fmt.Printf("%d. %s\n", index+1, task)
 	}
+
+	fmt.Printf("Max items are: %v\n", itemLimit)
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	var updatedTaskItems = append(taskItems, newTask)
+	// printTasks(updatedTaskItems, 23)
+	return updatedTaskItems
 
 }
